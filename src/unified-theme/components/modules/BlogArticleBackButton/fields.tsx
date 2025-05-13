e185a15f893a2eb95b765df905b94c66b0b04b92
root <root@deployment-mainline-6c6855464-zmcgm>
2025-05-13 20:06:06 +0000

    Removing files that were deleted in the private repo

--- src/unified-theme/components/modules/BlogArticleBackButton/fields.tsx
+++
@@ -1,8 +0,0 @@
-import { ModuleFields, TextField, LinkField } from '@hubspot/cms-components/fields';
-
-export const fields = (
-  <ModuleFields>
-    <TextField label="Back Button Text" name="text" default="Back to Blog" />
-    <LinkField label="Back Link URL" name="url" default={{}} />
-  </ModuleFields>
-);
